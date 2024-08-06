c32b2f1234d446d2f4922cac2ef12306af76fb85
aritonovicjovan <Aritonovicjovan@gmail.com>
2024-08-06 22:04:23 +0200

    feat(vite): update config for dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,36 @@
+import path from 'node:path';
 import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: { alias: { src: resolve('src/') } },
+  resolve: { alias: { '@src': resolve('./src') } },
+  plugins: [
+    tsConfigPaths({}),
+    dts({
+      insertTypesEntry: true,
+      include: ['*/*.ts'],
+    }),
+  ],
   build: {
-    lib: { entry: resolve(__dirname, 'src/index.ts'), formats: ['es'] },
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'react-jstore',
+      formats: ['es'],
+      fileName: 'index',
+    },
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        format: 'es',
+        dir: 'dist/',
+      },
+    },
   },
 });
