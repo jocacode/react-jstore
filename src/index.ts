ea480378a9194ad0d376a0a7031408ac419b19e5
aritonovicjovan <Aritonovicjovan@gmail.com>
2024-08-09 23:28:09 +0200

    fix(store): update types, add function description for store creating

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export { default as createStore } from './store';
+
+//Types
+export type {
+  SetState,
+  StoreCreator,
+  StoreListener,
+  StoreValue,
+} from './store';
--- src/store.ts
+++ src/store.ts
@@ -1,36 +1,43 @@
 import { useSyncExternalStore } from 'react';
 
-type StoreListener<Store> = (state?: Store) => void;
+export type StoreListener<Store> = (store?: Store) => void;
 
-type SetState<Store> = (
-  updater:
-    | Store
-    | Partial<Store>
-    | ((currentState: Store) => Store | Partial<Store>),
-) => void;
+export type StoreValue<Store> =
+  | Store
+  | Partial<Store>
+  | ((currentState: Store) => Store | Partial<Store>);
 
-type StoreCreator<Store> = (
+export type SetState<Store> = (updater: StoreValue<Store>) => void;
+
+export type StoreCreator<Store> = (
   setState: SetState<Store>,
   getState: () => Store,
 ) => Store;
 
+/**
+ * Function will create external store witch can be access across the app.
+ * @param {number} createState function with setter and getter methods
+ * for defining states and functions in external store.
+ * @return {Function} Result will be React hook for accessing and subscribing
+ * to an specified external state value.
+ */
 export const createStore = <Store extends Record<string, any>>(
   createState: StoreCreator<Store>,
 ) => {
   let state: Store;
   const listeners = new Set<StoreListener<Store>>();
 
-  const updateState = (value: any) => {
+  const updateState = (value: StoreValue<Store>) => {
     const newState = typeof value === 'function' ? value(state) : value;
     return Object.assign({}, state, newState);
   };
 
-  const setState: SetState<Store> = (value) => {
+  const setState: SetState<Store> = (value: StoreValue<Store>) => {
     state = updateState(value);
     listeners.forEach((listener: StoreListener<Store>) => listener(state));
   };
 
-  const getState = () => {
+  const getState = (): Store => {
     return state;
   };
 
